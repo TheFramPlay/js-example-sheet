2b639177fa06895cb873ba1fa17e7fa7f0f7bcac
TheFramPlay <harleyrgarcia@gmail.com>
2022-07-27 13:31:21 -0500

    Just adding notes on JS fundamentals

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 This is just an example sheet to use through the fundamentals of JS. {essentially a blank HTML file that we'll run scripts in the browser.}
 Read through the first section of JS fundamentals. I think this is going to be just a good refresher, to get to the intermediate stuff. :D
+
+7/27/2022: Not really many changes, this sheet is still mainly being used to run JS examples.
+
+Mostly everything in the .js file are notes that I've been taking.
---
+++ javascript.js
@@ -0,0 +1,94 @@
+// Total of 3 ways to create a variable in JS.
+// 1. var - old way of declaring a variable. (we'll learn more later).
+// 2. let - works very similar to var.
+// 2. const - declare variable with const to values that will not change.
+
+// let message; -->let(declare) message(variable);
+
+// message = 'Hello'; -->store the string 'Hello' in the variable
+// the "=" symbol is an assignment operator. Hence we are assigning the string 'Hello' to the variable message
+
+// alert(message);
+
+// So instead of declaring the variable first, then assigning a value to it. you can declare and assign a value to it all at once.
+
+// Task 1!
+// 1. Declare two variables: admin and name.
+// 2. Assign the value "John" to name.
+// 3. Copy the value from name to admin.
+// 4. Show the value of admin using alert (must output “John”).
+
+// let admin, name;
+// name = 'John';
+// admin = name;
+// alert(admin);
+
+// Task 2!
+// 1. Create a variable with the name of our planet. How would you name such a variable?
+// 2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+
+// let planet = 'Earth';
+// let currentUserName = 'John';
+
+// For capitalizations for const variables, only capitalize if the value is known prior to execution. lowercase for values that have not been evaluated yet.
+
+// console.log(23 + 97);
+
+// console.log(3 + 6 + 9 + 3 + 6 + 9)
+
+// console.log((4 + 6 + 9) / 77)
+
+// let a = 10;
+
+// console.log(a);
+
+// console.log(9 * a);
+
+// let b = 7 * a;
+
+// console.log(b);
+
+// const max = 57;
+// let actual = max - 13;
+// let percentage = actual / max;
+
+// let x = "100";
+// let y = "10";
+// let z = x / y;
+// console.log(z);
+
+// NaN (Not a Number)
+// let x = 100 / "Apple";
+// console.log(isNaN(x));
+
+// Infinity example
+// let myNumber = 2;
+// while (myNumber != Infinity) {
+//     console.log(myNumber = myNumber * myNumber);
+// };
+
+// let x = 2 / 0;
+// let y = -2 / 0;
+// console.log(x,y);
+
+// Hexadecimal
+// let x = 0xFF;
+// console.log(x);
+
+// let myNumber = 32;
+// console.log(myNumber.toString(32));
+// console.log(myNumber.toString(16));
+// console.log(myNumber.toString(12));
+// console.log(myNumber.toString(10));
+// console.log(myNumber.toString(8));
+// console.log(myNumber.toString(2));
+
+// let x = 300;
+// let y = 359;
+// console.log(x === y);
+
+
+
+
+
